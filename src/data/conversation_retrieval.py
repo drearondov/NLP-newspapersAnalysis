a94e4033e6646755430ea143919bad9c2c694587
drearondov <63360808+drearondov@users.noreply.github.com>
2023-02-03 16:57:10 -0500

    :sparkles: Sentiment analysis done. Inconclusive

---
+++ src/data/conversation_retrieval.py
@@ -0,0 +1,23 @@
+import requests
+
+from dotenv import dotenv_values, find_dotenv
+from urllib.parse import urlencode
+
+
+dotenv_path = find_dotenv()
+env_variables = dotenv_values(dotenv_path)
+
+BASE_DIR = env_variables["BASE_DIR"]
+BEARER_TOKEN = env_variables["BEARER_TOKEN"]
+
+
+headers = {"Authorization": f"Bearer {BEARER_TOKEN}"}
+
+query = {
+        "ids": "1547732445729918977",
+        "tweet.fields": "id,conversation_id",
+        }
+payload = urlencode(query, safe=",:")
+
+tweets_url = f"https://api.twitter.com/2/tweets"
+response = requests.get(tweets_url, headers=headers, params=payload)
